235e7200c9c732786ff2464f6c9ac0c61f4fabf2
Bo Xie <bxie33@gatech.edu>
2014-09-22 10:15:16 -0400

    cifar10

---
+++ gpu/winitfile_cifar10.py
@@ -0,0 +1,17 @@
+import numpy as n
+from gpumodel import *
+from scipy.io import loadmat
+
+checkpoint_file = 'pretrained_models/cifar10'
+layers_ = IGPUModel.load_checkpoint(checkpoint_file)['model_state']['layers']
+layers = {}
+for l in layers_:
+  layers[l['name']] = l
+
+def makew(name, idx, shape, params=None):
+  layer_name = str(params[0])
+  return layers[layer_name]['weights'][idx]
+
+def makeb(name, shape, params=None):
+  layer_name = str(params[0])
+  return layers[layer_name]['biases']
